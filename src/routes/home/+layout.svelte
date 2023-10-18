f1c5471e41625045573e307f568024da339738e2
Dana-Sarkawt <91015045+Dana-Sarkawt@users.noreply.github.com>
2023-10-18 09:01:46 +0300

    UI update

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -77,7 +77,7 @@ cart.subscribe((value) => {
 
 {#if $categoryWritable.categories && $itemsWritable.items}
 <main class="bg-slate-100 dark:bg-gray-950 w-full h-full">
-  <Drawer placement='right' transitionType="fly" transitionParams={transitionParamsRight} bind:hidden={hidden6} id='sidebar6' width="w-[40vh]">
+  <Drawer placement='right' transitionType="fly" transitionParams={transitionParamsRight} bind:hidden={hidden6} id='sidebar6' width="w-full">
     <div class='flex items-center'>
       <h5
         id="drawer-label"
@@ -120,20 +120,19 @@ cart.subscribe((value) => {
         <Button on:click={buyingItems}>Buy Now</Button>
       </div>
     </Drawer>
-    <Navbar let:hidden let:toggle class="fixed z-30">
-        <NavBrand href="/">
+    <Navbar let:hidden let:toggle class="fixed z-30 top-0 backdrop-blur-lg bg-[#ffffffb3] dark:bg-[#212121b3]">
+        <NavBrand href="/" class="w-full flex justify-center items-center">
           <img
             src="{kubakLogo}"
             class="mr-3 h-6 sm:h-9"
             alt="Kubak"
           />
-          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white uppercase">
+          <span class="self-center whitespace-nowrap text-md md:text-lg font-semibold dark:text-white uppercase">
             Kubak Online Shopping
           </span>
         </NavBrand>
-        <div class="flex md:order-2 xl:w-[50%] lg:w-[80%] lg:items-center md:w-full">
-            <Search class="h-full w-full">
-            </Search>
+        <div class="flex justify-between  w-full flex-wrap md:flex-nowrap items-center mt-5">
+            
           {#if screenWidth > 1280}
           <NavUl class="md:order-1">
             <NavLi href="/" class="flex flex-row items-center">
@@ -147,8 +146,14 @@ cart.subscribe((value) => {
               </button>
             </NavUl>
           {/if}
+          <div class="w-full flex justify-between">
           <NavHamburger on:click={toggle} />
-        </div>
+          <div class="flex justify-center items-center w-full md:flex-row-reverse">
+          <Search class="h-full w-full">
+          </Search>
+
+          
+       
         <DarkMode class="text-lg m-1">
           <svelte:fragment slot="lightIcon">
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M16 12.005a4 4 0 1 1-4 4a4.005 4.005 0 0 1 4-4m0-2a6 6 0 1 0 6 6a6 6 0 0 0-6-6ZM5.394 6.813L6.81 5.399l3.505 3.506L8.9 10.319zM2 15.005h5v2H2zm3.394 10.193L8.9 21.692l1.414 1.414l-3.505 3.506zM15 25.005h2v5h-2zm6.687-1.9l1.414-1.414l3.506 3.506l-1.414 1.414zm3.313-8.1h5v2h-5zm-3.313-6.101l3.506-3.506l1.414 1.414l-3.506 3.506zM15 2.005h2v5h-2z"/></svg>
@@ -157,6 +162,8 @@ cart.subscribe((value) => {
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M13.502 5.414a15.075 15.075 0 0 0 11.594 18.194a11.113 11.113 0 0 1-7.975 3.39c-.138 0-.278.005-.418 0a11.094 11.094 0 0 1-3.2-21.584M14.98 3a1.002 1.002 0 0 0-.175.016a13.096 13.096 0 0 0 1.825 25.981c.164.006.328 0 .49 0a13.072 13.072 0 0 0 10.703-5.555a1.01 1.01 0 0 0-.783-1.565A13.08 13.08 0 0 1 15.89 4.38A1.015 1.015 0 0 0 14.98 3Z"/></svg>
           </svelte:fragment>
         </DarkMode>
+      </div>
+      </div>
         <NavUl {hidden} class="w-full">
           <NavLi href="/" active={true}>Home</NavLi>
           <NavLi id="nav-menu1" class="cursor-pointer"><Chevron aligned>Categories</Chevron></NavLi>
@@ -174,10 +181,14 @@ cart.subscribe((value) => {
             <UserCircleSolid  class="mx-1" size="25"/>
             Account
           </NavLi>
-          <NavLi href="/" class="flex flex-row items-center">
-            <CartOutline  class="mx-1" size="25"/>
-            Cart</NavLi>
+
+          <button on:click={() => (hidden6 = false)}>
+            <NavLi  class="flex flex-row items-center">
+              <CartOutline  class="mx-1" size="25"/>
+              Cart</NavLi>
+            </button>
             {/if}
+            
           </NavUl>
         </Navbar>
         <div >
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -33,17 +33,17 @@ function removeItemFromCart(item: Items) {
 
 </script>
 
-<div class="p-12">
+<div class="container mx-auto mt-36 md:mt-44">
   <CardsImagesCarousel />
 </div>
-  <div class="flex justify-center w-full">
+  <div class="flex justify-center w-full mt-24">
     <Badge border color="blue" class="text-2xl font-bold text-gray-100 rounded-3xl" ><span class="dark:text-gray-50 text-gray-800 p-1 px-4">
       Categories
     </span></Badge>
   </div>
-<div class="flex flex-row justify-around flex-wrap">
+<div class="flex justify-center flex-wrap mt-5">
 {#each $categoryWritable.categories as category}
-    <Card padding="sm" class="m-2 rounded-[2rem]" color="blue" >
+    <Card class="m-2 rounded-2xl">
         <a href="/">
             <img class="p-4 rounded-t-lg object-cover w-40 h-32" src={category.category_image} alt="product 1" />
         </a>
@@ -59,32 +59,32 @@ function removeItemFromCart(item: Items) {
   </div>  
   
   
-  <div class="flex justify-center w-full mt-4">
+  <div class="flex justify-center w-full mt-32">
     <Badge border color="dark" class="text-2xl font-bold text-gray-100 rounded-3xl" ><span class="dark:text-gray-50 text-gray-800 p-1 px-4">
       Popular Items
     </span></Badge>
   </div>
   <!-- <h1 class="flex font-bold justify-center text-2xl tracking-wide text-gray-900 dark:text-white">Popular Items</h1> -->
-  <div class="grid xl:grid-cols-5 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center">
+  <div class="w-full container mx-auto flex justify-center items-center gap-px flex-wrap mt-5">
     {#if $cart}
     {#each $itemsWritable.items as items}
-    <Card padding="sm" class="m-2 flex flex-col justify-between items-center rounded-[3rem]" color="dark" >
+    <Card class="m-2 w-44 h-auto md:w-64 flex flex-col justify-between items-center rounded-2xl" color="dark" >
       <div class="flex justify-start items-start w-full">
         <HeartOutline size="32"/>
       </div>
       <a href="/">
-        <img class="p-4 rounded-t-lg object-contain w-64 h-36" src={items.item_image} alt="product 1" />
+        <img class="p-4 rounded-t-lg object-contain w-36 h-36" src={items.item_image} alt="product 1" />
       </a>
       <div class="px-5 pb-5 flex justify-start w-full flex-col">
         <a href="/">
-          <h5 class='text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+          <h5 class='text-lg text-center font-semibold tracking-tight text-gray-900 dark:text-white'>
             {items.name}
           </h5>
         </a>
-        <span class="text-xl font-medium ">{items.price} IQD</span>
+        <span class="text-md text-center font-medium ">{items.price} IQD</span>
       </div>
-      <div class="flex justify-center w-[18%] bg-blue-400 bg-opacity-700 rounded-full items-center transition-all duration-500" 
-     style="width: {$cart.some((i) => i.id === items.id) ? '50%' : '18%'}">
+      <div class="flex justify-center bg-blue-400 bg-opacity-700 rounded-full items-center transition-all duration-500" 
+     style="width: {$cart.some((i) => i.id === items.id) ? '75%' : '36px'}">
   {#if $cart.some((i) => i.id === items.id)}
   <div class="flex justify-between w-full">
     <button on:click={()=>{addItemToCart(items);
