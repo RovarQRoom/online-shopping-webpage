91692ffc6237b05366651f534994b6d70cbdd7e6
RovarQRoom <rovar.07000120@outlook.com>
2023-10-29 16:27:52 +0300

    Update Fix The User Authentication At Home

--- src/lib/Models/Requests/User.request.model.ts
+++ src/lib/Models/Requests/User.request.model.ts
@@ -1,9 +1,9 @@
-import type { User } from "firebase/auth";
+import type { ConfirmationResult, User } from "firebase/auth";
 import type { Users } from "../User.model";
 
 export interface CreateUser{
     user: User | null;
-    confirmationResult?: null;
+    confirmationResult?: ConfirmationResult;
     loading: boolean;
     data: Users | null;
     errorMessage: string | null;
--- src/lib/store/firebase-store/auth.firebase.store.ts
+++ src/lib/store/firebase-store/auth.firebase.store.ts
@@ -1,5 +1,7 @@
 import type { CreateUser } from '$lib/Models/Requests/User.request.model';
-import type { User } from 'firebase/auth';
+import { auth, database } from '$lib/firebase/firebase';
+import { signInWithPhoneNumber, type ApplicationVerifier, type User, type ConfirmationResult } from 'firebase/auth';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import { writable } from 'svelte/store';
 
 // Create a writable store with an initial value of null
@@ -10,14 +12,42 @@ export const createAuthStore = () => {
 	return {
 		subscribe,
 		set: (value: CreateUser) => set(value),
-		sign_in: (user: User) => {
+		sign_in: async (phoneNumber: string, applicationVerifier:ApplicationVerifier) => {
+			let confirmationResult = await signInWithPhoneNumber(auth ,phoneNumber, applicationVerifier);
+			if (confirmationResult) {
+				let user = authStore.get_user_by_phone_number(phoneNumber);
+				set({
+					user: user,
+					confirmationResult: confirmationResult,
+					loading: false,
+					data: null,
+					errorMessage: null
+				});
+			}
 		},
 		sign_out: () => {
 		},
-		sign_up: (user: User) => {
-		},
 		confirm: (code: string) => {
+		},
+		get_user_by_phone_number: async (phoneNumber: string) => {
+			try{
+				const queryUsers = query(
+				collection(database, 'users'), 
+				where('phone', '==', phoneNumber),
+				limit(1)
+				);
+
+			const querySnapshot = await getDocs(queryUsers);
+
+			const user = querySnapshot.docs.map((doc) => {
+				return { ...doc.data(), id: doc.id };
+			})[0];
+
+			return user;
+		}catch(e){
+			console.log("Error: ",e);
 		}
+	}
 	}
 };
 
