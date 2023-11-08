054a1d2e664baa5bec770585c77ef9b92bed6b23
Dana-Sarkawt <91015045+Dana-Sarkawt@users.noreply.github.com>
2023-11-08 10:39:43 +0300

    Update Added Store To Appwrite

---
+++ src/lib/store/appwrite-store/card-appwrite.store.ts
@@ -0,0 +1,16 @@
+import { Client, Databases, ID } from "appwrite";
+
+
+
+
+const promise = databases.createDocument(
+    '[654b2f6a8af9b2ed391f]',
+    '[654b3405853b2ef8aa7e]',
+    ID.unique(),
+    {
+        userId:"HelloTherew",
+        webpageUrl:"HeloMamam",
+        imageUrl:"Image",
+        expirationDate:new Date()
+}
+);
